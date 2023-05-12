a942376086a776b80bf62690352833b8ea4e53d2
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-05-12 21:29:33 +0000

    3 environment and bd configured

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -1,4 +1,4 @@
-import { getAllStudent } from "@/app/model/StudentService";
+import { addStudent, getAllStudent } from "@/app/model/StudentService";
 import { NextResponse } from "next/server";
 
 export async function GET() {
